1c3e2bf94650f6c9d612f32443310f17639c167d
woduseh <39576391+woduseh@users.noreply.github.com>
2019-05-24 12:27:28 +0900

    HW9 완료
    간단한 추가기능들

--- ChatClient.java
+++ ChatClient.java
@@ -2,7 +2,11 @@ import java.net.*;
 import java.io.*;
 
 public class ChatClient {
-
+	/* 우선 금지어를 string array로 만듬. 그 후  입력한 문장에 금지어가 포함되었는지 체크하는 boolean is_Ban을 만듬
+	 * for문을 통해서 입력한 문장에 금지어가  포함되어있는지 체크
+	 * 금지어가 포함되어있다면 어떤 금지어가 포함되어있는지 사용자에게 알려주고 메시지를 보내지 않음
+	 * 금지어가 포함되어있지 않다면 정상적으로 실행함  */
+	static String[] Banwords = {"학점", "성적", "안드로이드", "팀플", "조별과제"};
 	public static void main(String[] args) {
 		if(args.length != 2){
 			System.out.println("Usage : java ChatClient <username> <server-ip>");
@@ -13,6 +17,7 @@ public class ChatClient {
 		PrintWriter pw = null;
 		boolean endflag = false;
 		try{
+			boolean is_Ban = false; // is_Ban
 			sock = new Socket(args[1], 10001);
 			pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
 			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
@@ -24,12 +29,23 @@ public class ChatClient {
 			it.start();
 			String line = null;
 			while((line = keyboard.readLine()) != null){
-				pw.println(line);
-				pw.flush();
-				if(line.equals("/quit")){
-					endflag = true;
-					break;
-				}
+				for (String Ban: Banwords) {
+					if (line.contains(Ban)) {
+						System.out.println("'" + Ban + "'" + "은 금지어입니다!");
+						is_Ban = true;
+					}
+				} // 입력한 문장에 금지어가 포함되었는지 체크
+				if (is_Ban != true) {
+					pw.println(line);
+					pw.flush();
+					if(line.equals("/quit")){
+						endflag = true;
+						break;
+					}
+				} // 금지어가 포함되어있지 않을 경우 정상적으로 출력 진행
+				else {
+					is_Ban = false;
+				} // 금지어가 포함되어있다면 출력을 진행하지 않고 다시 금지어 체크 변수를 false로 변경
 			}
 			System.out.println("Connection closed.");
 		}catch(Exception ex){
--- ChatServer.java
+++ ChatServer.java
@@ -6,9 +6,10 @@ public class ChatServer {
 
 	public static void main(String[] args) {
 		try{
+			@SuppressWarnings("resource") // Server를 정상적으로 종료시키는 분기를 만드는 대신 임시로 경고를 제거함
 			ServerSocket server = new ServerSocket(10001);
 			System.out.println("Waiting connection...");
-			HashMap hm = new HashMap();
+			HashMap<String, PrintWriter> hm = new HashMap<String, PrintWriter>();
 			while(true){
 				Socket sock = server.accept();
 				ChatThread chatthread = new ChatThread(sock, hm);
@@ -24,9 +25,9 @@ class ChatThread extends Thread{
 	private Socket sock;
 	private String id;
 	private BufferedReader br;
-	private HashMap hm;
-	private boolean initFlag = false;
-	public ChatThread(Socket sock, HashMap hm){
+	private HashMap<String, PrintWriter> hm; // HashMap을 ChatThread에서 만드는 대신 남(main)이 만든 hm을 가져온다..!
+	//private boolean initFlag = false;
+	public ChatThread(Socket sock, HashMap<String, PrintWriter> hm){
 		this.sock = sock;
 		this.hm = hm;
 		try{
@@ -38,11 +39,11 @@ class ChatThread extends Thread{
 			synchronized(hm){
 				hm.put(this.id, pw);
 			}
-			initFlag = true;
+			//initFlag = true;
 		}catch(Exception ex){
 			System.out.println(ex);
 		}
-	} // construcor
+	} // Constructor
 	public void run(){
 		try{
 			String line = null;
@@ -51,8 +52,13 @@ class ChatThread extends Thread{
 					break;
 				if(line.indexOf("/to ") == 0){
 					sendmsg(line);
-				}else
+				}
+				if(line.equals("/userlist")){
+					send_userlist();
+				}
+				else {
 					broadcast(id + " : " + line);
+				}
 			}
 		}catch(Exception ex){
 			System.out.println(ex);
@@ -82,14 +88,32 @@ class ChatThread extends Thread{
 		}
 	} // sendmsg
 	public void broadcast(String msg){
+		// 현재 broadcast 하려는 pw가 자신의 pw와 동일한지 체크. 동일하지 않을 경우에만 채팅 문장을 출력하도록 변경.
 		synchronized(hm){
-			Collection collection = hm.values();
-			Iterator iter = collection.iterator();
+			Collection<PrintWriter> collection = hm.values();
+			Iterator<PrintWriter> iter = collection.iterator();
 			while(iter.hasNext()){
 				PrintWriter pw = (PrintWriter)iter.next();
-				pw.println(msg);
-				pw.flush();
+				if (pw!=hm.get(id)) {
+					pw.println(msg);
+					pw.flush();
+				}
 			}
 		}
 	} // broadcast
+	public void send_userlist() {
+		/* 우선 hm에서 모든 유저의 id를 hm.keyset을 통해 받아와서 set에 저장하고 set이 끝날 때까지 id를 꺼내오기 위해 set.iterator를 만들어둠
+		 * 그 후 자기 자신의 pw를 hm.get(id)를 통해 가져온 다음 iterator에 next가 없을 때까지 user의 번호와 아이디를 출력
+		 */
+		Set<String> set = hm.keySet();
+		Iterator<String> iterator = set.iterator();
+		PrintWriter pw = hm.get(id);
+		int i = 1;
+		while(iterator.hasNext()){
+			String username = (String)iterator.next();
+			pw.println("User#" + i + ": " + username);
+			pw.flush();
+			i++;
+		}
+	} // send_userlist
 }
